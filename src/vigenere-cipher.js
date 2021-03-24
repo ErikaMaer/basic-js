6db50960372547dce6663b4f759b951460b7340a
ErikaMaer <46538589+ErikaMaer@users.noreply.github.com>
2021-03-24 15:32:41 +0300

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,69 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(bool) {
+        this.bool = bool;
+    }
+
+    encrypt(message, key) {
+        let alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("")
+        message = message.toUpperCase();
+
+        key = this.getKey(message, key, alphabet)
+
+        let result = [];
+        for (let i = 0; i < message.length; i++) {
+            if (alphabet.includes(message[i])) {
+                result.push(alphabet[(alphabet.indexOf(message[i]) + alphabet.indexOf(key[0])) % 26])
+                key.shift();
+            } else {
+                result.push(message[i])
+            }
+        }
+
+        if (this.bool === false) {
+            return result.reverse().join('');
+        }
+        return result.join("");
+    }
+
+    decrypt(message, key) {
+        let alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("")
+        message = message.toUpperCase();
+
+        key = this.getKey(message, key, alphabet)
+
+        let result = [];
+        for (let i = 0; i < message.length; i++) {
+            if (alphabet.includes(message[i])) {
+                result.push(alphabet[(alphabet.indexOf(message[i]) - alphabet.indexOf(key[0]) + 26) % 26])
+                key.shift();
+            } else {
+                result.push(message[i])
+            }
+        }
+        if (this.bool === false) {
+            return result.reverse().join('');
+        }
+        return result.join("");
+    }
+
+    getKey(message, key, alphabet) {
+        let arr = []
+        for (let i = 0; i < message.length; i++) {
+            if (alphabet.includes(message[i])) {
+                arr.push(message[i])
+            }
+        }
+        arr = arr.join("")
+
+        let b;
+        if (arr.length > key.length) {
+            let a = key.repeat(Math.floor(arr.length / key.length))
+            b = a + key.slice(0, arr.length - a.length)
+        } else {
+            b = key.repeat(Math.ceil(arr.length / key.length))
+        }
+        return b.toUpperCase().split("")
+    }
 }
 
 module.exports = VigenereCipheringMachine;
